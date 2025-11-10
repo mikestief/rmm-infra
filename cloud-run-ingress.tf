dfc05a4473fcecfcc0f6c451dba04aaa36c2b24a
Mike Stief <mike_stief@yahoo.com>
2025-11-10 12:26:59 -0600

    Update syntax issue.

--- cloud-run-ingress.tf
+++ cloud-run-ingress.tf
@@ -6,13 +6,12 @@
 resource "google_cloud_run_v2_service" "default" {
   name     = var.cloud_run_service_name
   location = var.cloud_run_service_location
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
 
   template {
     # Preserve existing container configuration
     # The actual image and other settings should be managed via CI/CD
     # This resource primarily manages the ingress setting
-
-    ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
   }
 
   traffic {
