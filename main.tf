d400467735a3acb685d2bc611ce5cb5f7dde93c3
Mike Stief <mike_stief@yahoo.com>
2026-03-21 07:30:39 -0500

    feat: upgrade Terraform providers. Upgraded Google Cloud provider to ~> 6.0 and Random provider to ~> 3.6. Verified with terraform init -upgrade.

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1"
+      version = "~> 3.6"
     }
   }
 
